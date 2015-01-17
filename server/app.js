a8458cc8ad7cb2959a679bd128dc2788598e420a
podkolzzzin@yandex.ru <podkolzzzin@yandex.ru>
2015-01-18 01:17:30 +0200

    Net

--- server/app.js
+++ server/app.js
@@ -1,3 +1,24 @@
 /**
  * Created by podko_000 on 17.01.15.
  */
+
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(8080);
+
+function handler (req, res) {
+    fs.readFile(__dirname + '/index.html',
+        function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
+
+            res.writeHead(200);
+            res.end(data);
+        });
+}
+
+require("./server.js").bind(io);
---
+++ server/server.js
@@ -0,0 +1,82 @@
+/**
+ * Created by podko_000 on 17.01.15.
+ */
+var server = {
+
+    io: null,
+    games: [{hello: "world"}],
+    clients: [],
+
+    bind: function (io) {
+
+        this.io = io;
+        io.on("connection", server.onConnection);
+    },
+
+    onConnection: function (socket) {
+
+        console.log("onConnection");
+        server.clients.push(socket);
+        socket.on("addGame", server.onAddGame);
+        socket.on("joinGame", server.onJoinGame);
+        socket.on("getGames", server.onGetGames);
+        socket.emit("initial", {"id": server.clients.length - 1});
+        socket.emit("getGames", server.games);
+
+    },
+
+    onGetGames: function(data) {
+
+        console.log("onGetGames");
+        console.log(data.id);
+        server.clients[data.id].emit("getGames", server.lean(server.games));
+        console.log("data sent");
+    },
+
+    onAddGame: function(data) {
+
+        console.log("onAddGame");
+        data.host = server.clients[data.id];
+        data.hostId= data.id;
+        server.games.push(data);
+    },
+
+    onJoinGame: function(data) {
+
+      if(this.games[data.gameId]) {
+          var game = server.games[data.gameId];
+          game.started = true;
+          game.guest = server.clients[data.id];
+          game.guest = data.id;
+          game.host.emit("joinGame", data);
+          game.host.on("put", server.onPut);
+          game.guest.on("put", server.onPut);
+      }
+    },
+
+    onPut: function(data) {
+
+        if(server.games[data.gameId]) {
+            var game = server.games[data.gameId];
+            var s = game.hostId == data.id ? game.guest : game.host;
+            s.emit("put", data);
+        }
+    },
+
+    lean: function(gameData){
+
+        var r = [];
+        for (var i=0; i < gameData.length; i++) {
+            var g = gameData[i];
+            var obj = {};
+            for(var key in g) {
+                if(key != 'host' && key != 'guest')
+                    obj[key] = g[key];
+            }
+            r.push(obj);
+        }
+        return r;
+    }
+};
+
+module.exports = server;
